a01283365801f7012a41895d2cd782daa65b53c3
salvatore <salvatore@rezo.net>
2011-06-02 06:59:42 +0000

    langues (urls)

--- lang/urls_ar.php
+++ lang/urls_ar.php
@@ -8,33 +8,33 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'actualiser_toutes' => 'Mettre &agrave; jour toutes les urls', # NEW
+	'actualiser_toutes' => 'تحديث كل عناوين URL',
 
 	// I
-	'icone_controler_urls' => 'URLs signifiantes', # NEW
+	'icone_controler_urls' => 'عناوين URL ذات معنى ',
 
 	// L
-	'label_tri_date' => 'Date', # NEW
-	'label_tri_id' => 'ID', # NEW
-	'label_tri_url' => 'URL', # NEW
-	'label_url' => 'Nouvelle URL', # NEW
-	'label_url_permanente' => 'Verrouiller cette URL (pas de mise a jour apr&egrave;s edition de l\'objet)', # NEW
-	'liberer_url' => 'Liberer', # NEW
-	'liste_des_urls' => 'Toutes les URLs', # NEW
+	'label_tri_date' => 'التاريخ',
+	'label_tri_id' => 'رقم التسلسل',
+	'label_tri_url' => 'عنوان URL',
+	'label_url' => 'عنوان URL جديد',
+	'label_url_permanente' => 'إقفال هذا العنوان (لا تحديث بعد تحرير العنصر)',
+	'liberer_url' => 'إلغاء القفل',
+	'liste_des_urls' => 'كل عناوين URL',
 
 	// T
 	'texte_type_urls' => 'يمكنك اختيار أسلوب حساب عناوين الصفحات.',
 	'texte_type_urls_attention' => 'تنبيه، لن يعمل هذا الاعداد الا اذا كان ملف @htaccess@ مثبتاً بشكل سليم في أصل الموقع.',
 	'titre_type_urls' => 'نوع عناوين URL',
-	'tout_voir' => 'Voir toutes les URLs', # NEW
+	'tout_voir' => 'عرض كل عناوين URL',
 
 	// U
-	'url_ajout_impossible' => 'Une erreur est survenue. Il n\'a pas &t& possible d\'enregistrer cette URL', # NEW
-	'url_ajoutee' => 'L\'URL a &eacute;t&eacute; ajout&eacute;e', # NEW
+	'url_ajout_impossible' => 'حصل خطأ. لم يتم تسجيل هذا العنوان',
+	'url_ajoutee' => 'تمت إضافة هذا العنوان',
 
 	// V
-	'verifier_url_nettoyee' => 'L\'URL a &eacute;t&eacute; corrig&eacute;e, vous pouvez verifier avant de l\'enregistrer.', # NEW
-	'verrouiller_url' => 'Verrouiller' # NEW
+	'verifier_url_nettoyee' => 'تم تصحيح العنوان، يمكنك التأكد قبل تسجيله.',
+	'verrouiller_url' => 'إقفال'
 );
 
 ?>
