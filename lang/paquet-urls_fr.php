3c6c10c906b8c07ecf5212af9026abcbe9d50ecc
salvatore <salvatore@rezo.net>
2012-01-03 12:10:34 +0000

    langues (paquet-urls)

--- lang/paquet-urls_fr.php
+++ lang/paquet-urls_fr.php
@@ -1,18 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-urls
-// Langue: fr
-// Date: 30-07-2011 15:01:39
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/urls_etendues/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// U
+	// U
 	'urls_description' => 'Gestion des variantes d\'URL signifiantes ou non',
-	'urls_slogan' => 'Gestion des variantes d\'URL signifiantes ou non',
+	'urls_slogan' => 'Gestion des variantes d\'URL signifiantes ou non'
 );
+
 ?>
