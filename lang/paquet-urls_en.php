c7db7183a64848a877f948fd9ee48f3f29ab87b7
kent1 <kent1@arscenic.info>
2012-01-04 16:55:14 +0000

    langues (paquet-urls)

---
+++ lang/paquet-urls_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// U
+	'urls_description' => 'Management of the URL variants, meaningfull or not',
+	'urls_slogan' => 'Management of the URL variants, meaningfull or not'
+);
+
+?>
