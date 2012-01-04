b033044720443e3ffcd6ef4cc77b855c0cc0617d
salvatore <salvatore@rezo.net>
2012-01-04 16:55:13 +0000

    langues (urls)

--- lang/urls_en.php
+++ lang/urls_en.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.org
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -8,42 +8,63 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'actualiser_toutes' => 'Mettre &agrave; jour toutes les urls', # NEW
+	'actualiser_toutes' => 'Update all urls',
+
+	// B
+	'bouton_supprimer_url' => 'Delete this URL',
+
+	// E
+	'erreur_arbo_2_segments_max' => 'You can\'t use more than two URL segments for an object',
+	'explication_editer' => 'The advanced management of urls allows you to edit the url of the pages of each editorial object, and to manage the history of their evolution.',
 
 	// I
-	'icone_controler_urls' => 'URLs signifiantes', # NEW
+	'icone_configurer_urls' => 'Setup the URLs',
+	'icone_controler_urls' => 'Meaningfull URLs',
+	'info_1_url' => '1 URL',
+	'info_id_parent' => '#parent',
+	'info_nb_urls' => '@nb@ URLs',
+	'info_objet' => 'Object',
 
 	// L
-	'label_tri_date' => 'Date', # NEW
-	'label_tri_id' => 'ID', # NEW
-	'label_tri_url' => 'URL', # NEW
-	'label_url' => 'Nouvelle URL', # NEW
-	'label_url_permanente' => 'Verrouiller cette URL (pas de mise a jour apr&egrave;s edition de l\'objet)', # NEW
-	'liberer_url' => 'Liberer', # NEW
-	'liste_des_urls' => 'Toutes les URLs', # NEW
+	'label_tri_date' => 'Date',
+	'label_tri_id' => 'ID',
+	'label_tri_url' => 'URL',
+	'label_url' => 'New URL',
+	'label_url_minuscules_0' => 'Keep the title case',
+	'label_url_minuscules_1' => 'Forcing lowercase urls',
+	'label_url_permanente' => 'Lock this URL (no update after modification of the object)',
+	'label_url_sep_id' => 'Character to separate the number added in case of duplicate',
+	'label_urls_activer_controle_oui' => 'Enable advanced management of URLs',
+	'label_urls_nb_max_car' => 'Maximum number of characters',
+	'label_urls_nb_min_car' => 'Minimum number of characters',
+	'liberer_url' => 'Release',
+	'liste_des_urls' => 'All URLs',
 
 	// T
 	'texte_type_urls' => 'You may choose how page URLs are to be calculated.',
 	'texte_type_urls_attention' => 'Warning: this setting will only work if the file @htaccess@ is correctly installed in the site\'s root directory.',
-	'titre_type_arbo' => 'Arborescentes', # NEW
-	'titre_type_html' => 'Objets HTML', # NEW
-	'titre_type_libres' => 'Libres', # NEW
-	'titre_type_page' => 'Page', # NEW
-	'titre_type_propres' => 'Propres', # NEW
-	'titre_type_propres2' => 'Propres+<tt>.html</tt>', # NEW
-	'titre_type_propres_qs' => 'Propres en query-string', # NEW
-	'titre_type_simple' => 'Simples', # NEW
-	'titre_type_standard' => 'Historiques', # NEW
+	'texte_urls_nb_max_car' => 'If the title is longer, it will be cut.',
+	'texte_urls_nb_min_car' => 'If the title is shorter, it\'s its identifying number which will be used.',
+	'titre_gestion_des_urls' => 'URLs management',
+	'titre_type_arbo' => 'Arborescent URLs',
+	'titre_type_html' => 'HTML objects URLs ',
+	'titre_type_libres' => 'Free URLs',
+	'titre_type_page' => 'Page URLs',
+	'titre_type_propres' => 'Clean URLs',
+	'titre_type_propres2' => 'Clean+<tt>.html</tt>',
+	'titre_type_propres_qs' => 'Clean in query-string URLs',
+	'titre_type_simple' => 'Simple URLs',
+	'titre_type_standard' => 'Historical URLs',
 	'titre_type_urls' => 'Type of URL addresses',
-	'tout_voir' => 'Voir toutes les URLs', # NEW
+	'tout_voir' => 'Show all URLs',
 
 	// U
-	'url_ajout_impossible' => 'Une erreur est survenue. Il n\'a pas &t& possible d\'enregistrer cette URL', # NEW
-	'url_ajoutee' => 'L\'URL a &eacute;t&eacute; ajout&eacute;e', # NEW
+	'url_ajout_impossible' => 'An error has occurred. This URL couldn\'t be saved',
+	'url_ajoutee' => 'The URL has been added',
 
 	// V
-	'verifier_url_nettoyee' => 'L\'URL a &eacute;t&eacute; corrig&eacute;e, vous pouvez verifier avant de l\'enregistrer.', # NEW
-	'verrouiller_url' => 'Verrouiller' # NEW
+	'verifier_url_nettoyee' => 'The URL has been corrected, you can check before saving.',
+	'verrouiller_url' => 'Lock'
 );
 
 ?>
