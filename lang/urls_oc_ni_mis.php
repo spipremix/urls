5fb3328dd66a290ddafed93bdd054be337a44555
spip <spip@oliver-fr.org>
2015-03-12 04:34:25 +0000

    [Salvatore] [source:_core_/plugins/urls_etendues/lang/ urls] Export depuis http://trad.spip.net de la langue oc_ni_mis

---
+++ lang/urls_oc_ni_mis.php
@@ -0,0 +1,62 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/urls?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'actualiser_toutes' => 'Metre a jou touti li URL',
+
+	// B
+	'bouton_supprimer_url' => 'Suprimà aquela URL',
+
+	// E
+	'erreur_arbo_2_segments_max' => 'Noun poudès utilisà mai de doui segment d’URL per un ouget',
+
+	// I
+	'icone_configurer_urls' => 'Counfigurà li URL',
+	'icone_controler_urls' => 'URL significativi',
+	'info_1_url' => '1 URL',
+	'info_id_parent' => '#parent',
+	'info_nb_urls' => '@nb@ URL',
+	'info_objet' => 'Ouget',
+
+	// L
+	'label_tri_date' => 'Data',
+	'label_tri_id' => 'ID',
+	'label_tri_url' => 'URL',
+	'label_url' => 'URL nouvela',
+	'label_url_minuscules_1' => 'Fouòrça li URL en minùscula',
+	'label_url_permanente' => 'Ferouià aquela URL (mìnga metuda a jou après edicioun de l’ouget)',
+	'label_urls_activer_controle_oui' => 'Ativà la gestioun avançada dei URL',
+	'label_urls_nb_max_car' => 'Noumbre massimoum de caracter',
+	'label_urls_nb_min_car' => 'Noumbre minimum de caracter',
+	'liberer_url' => 'Liberà',
+	'liste_des_urls' => 'Touti li URL',
+
+	// T
+	'texte_type_urls' => 'Poudès chausì aquì souta lou mòdou de carcul de l’adressa dei pàgina.',
+	'texte_urls_nb_max_car' => 'Se lou titre es mai lonc, serà coupat.',
+	'titre_gestion_des_urls' => 'Gestioun dei URL',
+	'titre_type_html' => 'URL Ouget HTML',
+	'titre_type_libres' => 'URL Lìberou',
+	'titre_type_page' => 'URL Pàgina',
+	'titre_type_propres' => 'URL Propri',
+	'titre_type_propres2' => 'URL Propri+<tt>.html</tt>',
+	'titre_type_propres_qs' => 'URL Propri en query-string',
+	'titre_type_simple' => 'URL Simpli',
+	'titre_type_standard' => 'URL Istòriqui',
+	'titre_type_urls' => 'Tìpou d’adressa URL',
+	'tout_voir' => 'Vèire touti li URL',
+
+	// U
+	'url_ajoutee' => 'L’URL es estada ajustada',
+
+	// V
+	'verrouiller_url' => 'Ferouià'
+);
+
+?>
