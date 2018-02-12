e3f6ef6829e437ac18f98942c87c974cf533626f
nolwenn.berbey <nolwenn.berbey@gmail.com>
2018-02-12 04:36:17 +0000

    [Salvatore] [source:_core_/plugins/urls_etendues/lang/ paquet-urls] Export depuis http://trad.spip.net de la langue br

---
+++ lang/paquet-urls_br.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-urls?lang_cible=br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// U
+	'urls_description' => 'Merañ an adstummoù URL sklaer pe ket',
+	'urls_slogan' => 'Merañ an adstummoù URL sklaer pe ket'
+);
