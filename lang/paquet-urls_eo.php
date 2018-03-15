499e25c18f4028ac085c4c7ded8860536492f450
gpl <gpl@long-player.net>
2018-03-15 04:42:18 +0000

    [Salvatore] [source:_core_/plugins/urls_etendues/lang/ paquet-urls] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-urls_eo.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-urls?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// U
+	'urls_description' => 'Mastrumado de aliformaj retadresoj (URL), sencaj aŭ ne',
+	'urls_slogan' => 'Mastrumado de aliformaj retadresoj (URL), sencaj aŭ ne'
+);
