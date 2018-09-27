d3255e840e99b52244997edea7e77226bdb23fc1
Cerdic <cedric@yterium.com>
2018-09-27 14:06:32 +0000

    Fix https://core.spip.net/issues/4115 : eviter une collision de nommage dans ieconfig (est-ce que ca a sa place ici, vraiment?)

--- urls_ieconfig.php
+++ urls_ieconfig.php
@@ -5,10 +5,10 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 function urls_ieconfig_metas($table) {
-	$table['urls']['titre'] = _T('urls:titre_type_urls');
-	$table['urls']['icone'] = 'url-16.png';
-	$table['urls']['metas_brutes'] = 'type_urls,urls_activer_controle';
-	$table['urls']['metas_serialize'] = 'urls_propres,urls_arbo';
+	$table['urls_meta']['titre'] = _T('urls:titre_type_urls');
+	$table['urls_meta']['icone'] = 'url-16.png';
+	$table['urls_meta']['metas_brutes'] = 'type_urls,urls_activer_controle';
+	$table['urls_meta']['metas_serialize'] = 'urls_propres,urls_arbo';
 
 	return $table;
 }
